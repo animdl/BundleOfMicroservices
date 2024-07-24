ad456a769eb9e04625f045fb140057c5a74e3784
Animdl <animdl.gh@gmail.com>
2024-07-24 14:29:36 -0500

    use go-chi router to handle requests

--- two/main.go
+++ two/main.go
@@ -3,20 +3,32 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
+	// chi router manages the handler
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+	// sets the route and handler to point to
+	router.Get("/hello", basicHandler)
+
+	// sets the server to listen to requests from the port and use the chi router as the handler
 	server := &http.Server{
 		Addr:    ":3000",
-		Handler: http.HandlerFunc(basicHandler),
+		Handler: router,
 	}
 
+	// if there is a faulty request, print to console
 	err := server.ListenAndServe()
 	if err != nil {
 		fmt.Println("Failed to listen to server", err)
 	}
 }
 
+// basicHandler responds to requests with a json response
 func basicHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, from the handler!"))
+	w.Write([]byte("Hello, from the handler using go-chi!"))
 }
